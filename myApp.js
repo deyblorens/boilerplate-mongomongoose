38c3161daa71ec134d01f3e3b224e55609bfc54d
deyblorens <davelorencelepaopao@gmail.com>
2024-08-13 18:18:59 +0000

    Connect to MongoDB

--- myApp.js
+++ myApp.js
@@ -1,6 +1,13 @@
-require('dotenv').config();
+require("dotenv").config();
+const mongoose = require("mongoose");
 
+const { MONGO_URI } = process.env;
 
+//Connect to MongoDB
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true }, { useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected succesfully"))
+  .catch((err) => console.log("MongoDB connection error:", err));
 let Person;
 
 const createAndSavePerson = (done) => {
